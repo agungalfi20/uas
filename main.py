03a961440d9db5e11e14cc7d346f576baffd4dd6
agungalfi20 <agungalfi20@gmail.com>
2020-01-19 09:56:39 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+class MainApp():
+    def __init__(self):
+        self.books = []
+
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
+from core.baseapp import BaseApp
+from view.view_book import  *
+class MainApp(BaseApp)
+    def __init__(self):
+        self.books =[]
+     def list_book(self):
+         self.list_book()
+
+ if __name__ == "__main__":
+     app = MainApp()
+     app.run()
+
